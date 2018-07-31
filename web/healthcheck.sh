4561f143671b18769bb66fa64703247ac84524cf
youngblood <944168348@qq.com>
2018-07-31 18:21:29 +0800

    web

--- web/healthcheck.sh
+++ web/healthcheck.sh
@@ -6,8 +6,14 @@ if ! hash curl 2>/dev/null; then
 	apk add --no-cache curl
 fi
 
+p=$port
+
+if ["$p" = ''];then 
+	p='8080'
+fi
+
 # healthcheck
-if ping="$(curl  -X POST  http://127.0.0.1:$port/api/ping)" && [ "$ping" = 'pong' ]; then
+if ping="$(curl  -X POST  http://127.0.0.1:$p/api/ping)" && [ "$ping" = 'pong' ]; then
 	exit 0
 fi
 exit 1
